61abaec5b3e42ba3eee4c36173e27d46b522d3ac
vinaonepai <viniciusouza443@gmail.com>
2026-03-31 14:19:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SmartEvent',
+  webDir: 'dist'
+};
+
+export default config;
